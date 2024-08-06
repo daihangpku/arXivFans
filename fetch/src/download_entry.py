f032cb078d1e52f610f232ea44139e2db6a13382
Dai Hang <167784753+daihangpku@users.noreply.github.com>
2024-08-06 12:30:02 +0800

    Update download_entry.py

--- fetch/src/download_entry.py
+++ fetch/src/download_entry.py
@@ -23,9 +23,9 @@ def entry(results,keywords=[]):
     os.makedirs(filepath, exist_ok=True)
     results_str = [convert_datetime_to_str(result) for result in results]
     if os.path.isfile(os.path.join(filepath,nowtime+s)):
-        print(f"今日已经更新")
+        print(f"already updated today")
     else:
         with open(os.path.join(filepath,nowtime+s), 'w', encoding='utf-8') as file:
             json.dump(results_str, file, ensure_ascii=False, indent=4)
-        print(f"今日更新完毕")
+        print(f"successfully updated today")
     
