4ad12861d18eccd75cefe63664d08491c4a509ae
Dai Hang <167784753+daihangpku@users.noreply.github.com>
2024-08-07 16:11:32 +0800

    Update fetch.py

--- fetch/src/fetch.py
+++ fetch/src/fetch.py
@@ -2,12 +2,51 @@ import arxiv
 from datetime import datetime, timedelta
 import json
 import os
-def fetch_arxiv_updates(categories, keywords, days = 7):
-    search_query = ' OR '.join([f'cat:{cat}' for cat in categories])
-    search = arxiv.Search(search_query,max_results = 1000,sort_by = arxiv.SortCriterion.SubmittedDate)
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+import multiprocessing
+from .download import down_load
+
+def create_session_with_proxy(proxy):
+    session = requests.Session()
+    session.proxies.update(proxy)
     
+    retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[502, 503, 504])
+    session.mount('http://', HTTPAdapter(max_retries=retries))
+    session.mount('https://', HTTPAdapter(max_retries=retries))
+    
+    return session
+
+def multi_processing(results, categories, keywords, proxy):
+    processes = []
+    for category in categories:
+        p = multiprocessing.Process(target=down_load, args=(results, keywords, proxy))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+
+def fetch_arxiv_updates(categories, keywords, proxy, days = 7):
+    search_query = ' OR '.join([f'cat:{cat}' for cat in categories])
+    if not proxy == " ":
+        proxies = {
+            'http': f'{proxy}',
+            'https': f'{proxy}',
+        }
+        print("using proxy")
+        session_with_proxy = create_session_with_proxy(proxies)
+        arxiv.Client._session = session_with_proxy
+
+    search = arxiv.Search(search_query,max_results = 1000,sort_by = arxiv.SortCriterion.SubmittedDate)
+    print("searching finished")
     results = []
+    result = []
+    cnt = 0
     for passage in search.results():
+        cnt += 1
         if passage.updated.date()>datetime.now().date()-timedelta(days=days):
             if any(keyword.lower() in passage.summary.lower() for keyword in keywords):
                 results.append({
@@ -19,25 +58,62 @@ def fetch_arxiv_updates(categories, keywords, days = 7):
                     'category': categories,
                     'keyword': [keyword for keyword in keywords if keyword.lower() in passage.summary.lower()]
                 })
-    
+                result.append({
+                    "title": passage.title,
+                    "authors": [str(author) for author in passage.authors],
+                    'published': passage.published,
+                    'link': passage.entry_id,
+                    'summary': passage.summary,
+                    'category': categories,
+                    'keyword': [keyword for keyword in keywords if keyword.lower() in passage.summary.lower()]
+                })
+        if(cnt%100 == 0):
+            multi_processing(result, categories, keywords, proxy)
+            result = []
+    print("process finished")
     return  results
 
-def load_papers_from_json(file_path):
-    nowtime = datetime.now().date()
-    
-    os.makedirs(file_path, exist_ok=True)
+def load_papers_from_db(file_path):
     papers = []
-    all_files_and_dirs = os.listdir(file_path)
-    all_files = [f for f in all_files_and_dirs if os.path.isfile(os.path.join(file_path, f)) and str(nowtime) in f]
-    if len(all_files) == 0:
-        return False
-    for f in all_files:
-        with open(os.path.join(file_path, f), 'r', encoding='utf-8') as file:
-            paper = json.load(file)
-            for p in paper:
-                papers.append(p)
+    import sqlite3
+# 数据库文件路径
+    db_path = 'download.db'
+
+# 确认数据库文件存在
+    if not os.path.exists(db_path):
+        print(f"Database file does not exist at: {db_path}")
+    else:
+        try:
+            conn = sqlite3.connect(db_path)
+            cursor = conn.cursor()
+            #keyword = 'radiance field' 
+            query = f"SELECT * FROM papers "
+
+            cursor.execute(query)
+            rows = cursor.fetchall()
+
+            results=[]
+            for row in rows:
+                results.append({
+                    "title": row[1],
+                    'published': row[4],
+                    'link': row[0],
+                    'keyword': json.loads(row[2]),
+                    'local_link': row[3]
+                })
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
+        finally:
+            # 关闭连接
+            if conn:
+                conn.close()
+
+
+    #if len(rows) == 0:
+       # return False
+    
             
-    return papers
+    return results
         
     
         
