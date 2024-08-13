86ed5986bd48ab624959986de7e0781341f4b9c4
Dai Hang <167784753+daihangpku@users.noreply.github.com>
2024-08-13 10:54:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arXivFans',  
-    version='0.2.3',
+    version='0.3.0',
     description='A paper fetcher',
     url='https://github.com/daihangpku/arXivFans.git',  
     author='Daihang',
